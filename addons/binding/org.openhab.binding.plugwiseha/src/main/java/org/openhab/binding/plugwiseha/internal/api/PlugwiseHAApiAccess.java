e04e28522d963bf5fc1f63da6993898b531099c8
Kaj Visser <git@kajvisser.nl>
2019-02-23 19:54:07 +0100

    [plugwiseha] xstream error debug

--- addons/binding/org.openhab.binding.plugwiseha/src/main/java/org/openhab/binding/plugwiseha/internal/api/PlugwiseHAApiAccess.java
+++ addons/binding/org.openhab.binding.plugwiseha/src/main/java/org/openhab/binding/plugwiseha/internal/api/PlugwiseHAApiAccess.java
@@ -34,10 +34,10 @@ public class PlugwiseHAApiAccess {
 
     public PlugwiseHAApiAccess(PlugwiseHAConfiguration configuration, HttpClient httpClient) {
         StaxDriver driver = new StaxDriver();
+
         this.xstream = new XStream(driver);
 
         this.gson = new GsonBuilder().create();
-        // this.xstream = new XStream();
         this.httpClient = httpClient;
         this.configuration = configuration;
     }
